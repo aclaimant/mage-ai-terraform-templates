6b9a3bbf63143807324849f9eeba963316757e1d
Joel Friedman <joel@aclaimant.com>
2023-12-21 09:54:32 -0600

    Per Slack converation with Mage-api moving health check
    * `/api/kernels` is now behind auth and no longer works as a
      successful health check endpoint

--- aws/alb.tf
+++ aws/alb.tf
@@ -61,7 +61,7 @@ resource "aws_lb_target_group" "target_group" {
     protocol            = "HTTP"
     matcher             = "200"
     timeout             = "5"
-    path                = "/api/kernels"
+    path                = "/api/statuses"
     unhealthy_threshold = "2"
   }
 
