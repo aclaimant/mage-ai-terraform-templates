ce91527ce7b63bc227fb3f91c398d04aaca36085
Joel Friedman <joel@aclaimant.com>
2024-01-26 09:41:52 -0600

    Fixing typo

--- aws/main.tf
+++ aws/main.tf
@@ -54,7 +54,7 @@ data "template_file" "env_vars" {
     database_connection_url = "postgresql+psycopg2://${var.database_user}:${var.database_password}@${aws_db_instance.rds.address}:5432/mage"
     ec2_subnet_id           = aws_subnet.public[0].id
     acl_dbt_db_env          = var.ACL_DBT_DB_ENV
-    snowflake_database      = var.SNOWFLAKE_DATABSE
+    snowflake_database      = var.SNOWFLAKE_DATABASE
     snowflake_role          = var.SNOWFLAKE_ROLE
     snowflake_warehouse     = var.SNOWFLAKE_WAREHOUSE
     mage_public_host        = var.MAGE_PUBLIC_HOST
